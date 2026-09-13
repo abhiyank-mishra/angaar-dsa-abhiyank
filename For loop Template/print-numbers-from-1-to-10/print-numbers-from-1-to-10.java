90c52033736bbbb4988a17ae5c61d3f1e0b4777a
Abhiyank Mishra <182095768+abhiyank-mishra@users.noreply.github.com>
2026-09-13 23:13:04 +0530

    Solve: Print Numbers from 1 to 10 | LB [Easy] - The Angaar Batch

--- For loop Template/print-numbers-from-1-to-10/print-numbers-from-1-to-10.java
+++ For loop Template/print-numbers-from-1-to-10/print-numbers-from-1-to-10.java
@@ -1,8 +1,6 @@
  // Function Implementation
     public static void printOneToTen() {
          
-     // Standard Range Loop Template
-for (int i = 1; i <= 10; i++) {
-    System.out.println(i);
-}   
+         //Code here
+         
     }
